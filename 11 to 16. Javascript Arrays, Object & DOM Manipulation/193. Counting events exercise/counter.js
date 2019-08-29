4620dfae948e7b13fd25b74896f7de35315e7637
meijboom <dennis.meijboom@hotmail.com>
2019-08-29 19:06:31 +0200

    refractor and start of the color game

---
+++ 11 to 16. Javascript Arrays, Object & DOM Manipulation/193. Counting events exercise/counter.js
@@ -0,0 +1,7 @@
+// Count all the events on MDN Event reference
+
+// inspected element to find out all events are in <code> brackets.
+
+var count = document.getElementsByTagName("tr")
+
+// count is now == 810
---
+++ 17. Color Game App/cga.js
@@ -0,0 +1,15 @@
+var colors = [
+    "rgb(255,  0,  0)",
+    "rgb(255,255,  0)",
+    "rgb(  0,255,  0)",
+    "rgb(  0,255,255)",
+    "rgb(  0,  0,255)",
+    "rgb(255,  0,255)"
+]
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay")
+
+for(var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+}
